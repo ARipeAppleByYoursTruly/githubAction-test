52e6ee30a235708521aecb505a40288f4298a56c
ARipeAppleByYoursTruly <appleandroid1104@gmail.com>
2024-03-18 21:53:26 +0800

    Update generate-repoData.js

--- .github/workflows/generate-repoData.js
+++ .github/workflows/generate-repoData.js
@@ -3,6 +3,6 @@ import fs from "node:fs"
 
 
 export default async function main() {
-  const input = fs.readFileSync("./repoData/input.json")
+  const input = fs.readFileSync("./repoData/input.json", "utf-8")
   console.dir(input, {depth: null, maxArrayLength: null, maxStringLength: null})
 }

07c719e3292919524c69881b7849a5a0d183b863
ARipeAppleByYoursTruly <appleandroid1104@gmail.com>
2024-03-20 09:57:38 +0800

    asd

--- .github/workflows/generate-repoData.js
+++ .github/workflows/generate-repoData.js
@@ -4,8 +4,9 @@ import fs from "node:fs"
 
 export default async function main({github, context}) {
   const ownerName = context.repo.owner
-  const {repo_otherStuff_input} = JSON.parse(fs.readFileSync("./repoData/input.json", "utf-8"))
+  const {repoData_otherStuff_input} = JSON.parse(fs.readFileSync("./repoData/input.json", "utf-8"))
 
+  // Get user's list of repos
   const repoList = await (async () => {
     const repoList_response = await github.rest.repos.listForUser({
       username: ownerName,
@@ -21,5 +22,28 @@ export default async function main({github, context}) {
     }
   })()
 
-  console.dir(repoList, {depth: null, maxArrayLength: null, maxStringLength: null})
+
+
+  // const repoData = {
+  //   hundredPercentGuides: [],
+  //   otherStuff: []
+  // }
+
+  const repoData = (() => {
+    const asd = fs.readFileSync("./repoData/output.json", "utf-8")
+    console.dir(asd, {depth: null, maxArrayLength: null, maxStringLength: null})
+
+    return {
+      hundredPercentGuides: [],
+      otherStuff: []
+    }
+  })()
+
+  // Generate repoData for 100% guides
+  // To be implemened when I've started making 100%% guides
+
+  // Generate repoData for other stuff
+  for (const repoName of repoData_otherStuff_input) {
+    // const repoName
+  }
 }

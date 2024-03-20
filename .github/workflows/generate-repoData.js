aff1ceec969e4dd66f3c0be841c6fd1ae7bebe84
ARipeAppleByYoursTruly <appleandroid1104@gmail.com>
2024-03-20 09:00:05 +0800

    Update generate-repoData.js

--- .github/workflows/generate-repoData.js
+++ .github/workflows/generate-repoData.js
@@ -6,16 +6,20 @@ export default async function main({github, context}) {
   const ownerName = context.repo.owner
   const {repo_otherStuff_input} = JSON.parse(fs.readFileSync("./repoData/input.json", "utf-8"))
 
-  const repoList_response = await github.rest.repos.listForUser({
-    username: ownerName,
-    sort: "pushed",
-    per_page: 100
-  })
+  const repoList = await (async () => {
+    const repoList_response = await github.rest.repos.listForUser({
+      username: ownerName,
+      sort: "pushed",
+      per_page: 100
+    })
 
-  if (repoList_response.status !== 200) {
-    throw new Error("repoList_response.status is not 200")
-  }
+    if (repoList_response.status !== 200) {
+      throw new Error("repoList_response.status is not 200")
+    }
+    else {
+      return repoList_response.data
+    }
+  })()
 
-  const repoList = repoList_response.data
   console.dir(repoList, {depth: null, maxArrayLength: null, maxStringLength: null})
 }

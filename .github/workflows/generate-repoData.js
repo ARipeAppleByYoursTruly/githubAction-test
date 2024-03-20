c2ca3590616a92b739bbaf6ce94f1be9a79c3c09
ARipeAppleByYoursTruly <appleandroid1104@gmail.com>
2024-03-20 08:51:41 +0800

    asd

--- .github/workflows/generate-repoData.js
+++ .github/workflows/generate-repoData.js
@@ -4,13 +4,13 @@ import fs from "node:fs"
 
 export default async function main({github, context}) {
   const ownerName = context.repo.owner
-  const {repoNames} = JSON.parse(fs.readFileSync("./repoData/input.json", "utf-8"))
+  const {repo_otherStuff_input} = JSON.parse(fs.readFileSync("./repoData/input.json", "utf-8"))
 
-  const repoList_response = await github.rest.repos.listForUser({
+  const repoList = await github.rest.repos.listForUser({
     username: ownerName,
     sort: "pushed",
     per_page: 100
-  })
+  }).data
 
-  console.dir(repoList_response, {depth: null, maxArrayLength: null, maxStringLength: null})
+  console.dir(repoList, {depth: null, maxArrayLength: null, maxStringLength: null})
 }
